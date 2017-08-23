2e79869b801c2f700a398393eb34819b1726d31a
Ridder Janssen <ridderjanssen@gmail.com>
2017-08-23 22:55:50 +0200

    Added Form for making a new Toernooi

--- app/controllers/toernooi_controller.rb
+++ app/controllers/toernooi_controller.rb
@@ -1,2 +1,19 @@
 class ToernooiController < ApplicationController
+  def new
+    @toernooi = Toernooi.new
+  end
+  def create
+    @toernooi = Toernooi.new(toernooi_params)
+    if @toernooi.save
+      redirect_to :action => "new" #Waarheen?!
+    else
+      render :action => "new"
+    end
+  end
+
+  private
+
+    def toernooi_params
+      params.require(:toernooi).permit(:naam, :wedstrijdleider, :datum)
+    end
 end
