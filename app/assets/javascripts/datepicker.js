85fc17e57b005801cb2ee3abcc29eb4cec4ae2e5
Floyd van Boksel <floyd.van.boksel@gmail.com>
2017-11-07 21:16:00 +0100

    Change new tournament page styling

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Vandaag',
+    clear: 'Wissen',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
+}, false);
