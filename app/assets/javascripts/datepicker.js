7b6844a799585b601903f26d39b1e8de05c42779
Floyd van Boksel <floyd.van.boksel@gmail.com>
2017-11-21 20:13:15 +0100

    Fix datepicker not working if its not the initial page loaded

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("turbolinks:load", function() {
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -7,4 +7,4 @@ document.addEventListener('DOMContentLoaded', function(){
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
-}, false);
+});
